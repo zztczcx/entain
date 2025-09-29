2e681b7aeee952c754401d5b3f3c5691f01efd31
joezhao <chzhao@seek.com.au>
2025-09-29 10:32:26 +1000

    clean up

--- sports/tools.go
+++ sports/tools.go
@@ -10,5 +10,3 @@ import (
 	_ "google.golang.org/grpc/cmd/protoc-gen-go-grpc"
 	_ "google.golang.org/protobuf/cmd/protoc-gen-go"
 )
-
-//go:generate protoc --go_out=. --go_opt=paths=source_relative --go-grpc_out=. --go-grpc_opt=paths=source_relative proto/sports/sports.proto
